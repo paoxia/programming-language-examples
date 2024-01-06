040f9979c5438e1f18ffb56e961e4feeffe91b9d
ShengyunYu <yushengyun2013@163.com>
2024-01-06 23:07:32 +0800

    guava

--- src/main/java/org/saltedfish/thirdpartylibraries/guava/GuavaBloomFilterDemo.java
+++ src/main/java/org/saltedfish/thirdpartylibraries/guava/GuavaBloomFilterDemo.java
@@ -1,10 +1,26 @@
 package org.saltedfish.thirdpartylibraries.guava;
 
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
 /**
  *
  * @author SaltedFish
  * @date 2021/1/16
  */
 public class GuavaBloomFilterDemo {
+    public static void main(String[] args) {
+        BloomFilter<Integer> bloomFilter = BloomFilter.create(
+                Funnels.integerFunnel(),
+                1000,
+                0.01
+        );
 
+        bloomFilter.put(Integer.valueOf(1));
+        bloomFilter.put(Integer.valueOf(2));
+        bloomFilter.put(Integer.valueOf(3));
+
+        System.out.println(bloomFilter.mightContain(Integer.valueOf(1)));
+        System.out.println(bloomFilter.mightContain(Integer.valueOf(4)));
+    }
 }
--- src/main/java/org/saltedfish/thirdpartylibraries/guava/GuavaCacheDemo.java
+++ src/main/java/org/saltedfish/thirdpartylibraries/guava/GuavaCacheDemo.java
@@ -1,9 +1,26 @@
 package org.saltedfish.thirdpartylibraries.guava;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author SaltedFish
  *
  */
 public class GuavaCacheDemo {
+    public static void main(String[] args) {
+        Cache<String, Integer> cache = CacheBuilder.newBuilder()
+                .maximumSize(100)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build();
 
+        cache.put("key1", 1);
+        cache.put("key2", 2);
+        cache.put("key3", 3);
+
+        System.out.println(cache.getIfPresent("key1"));
+        System.out.println(cache.getIfPresent("key5"));
+    }
 }
