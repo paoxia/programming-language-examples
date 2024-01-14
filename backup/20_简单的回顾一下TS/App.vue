d9280ebd4b36f2a0fa1da422a835074f59546a5a
ShengyunYu <yushengyun2013@163.com>
2024-01-14 12:35:05 +0800

    简单回顾ts

---
+++ backup/20_简单的回顾一下TS/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+
+<script lang="ts" setup name="App">
+import Person from './components/Person.vue'
+
+
+</script>
+
+<style>
+.app {
+  background-color: blue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px
+}
+</style>
---
+++ backup/20_简单的回顾一下TS/types/index.ts
@@ -0,0 +1,9 @@
+// 定义接口,用于限制person对象的具体属性
+export interface PersonInter {
+    id:string,
+    name:string,
+    age:number
+}
+
+// export type Persons = Array<PersonInter>
+export type Persons = PersonInter[]
