bfcf35d39400a762125b1dd05682d3602bf6e750
ShengyunYu <yushengyun2013@163.com>
2024-01-14 11:25:48 +0800

    watch

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,76 +1,49 @@
 <template>
     <div class="person">
-        <h2>name : {{ person.name }}</h2>
-        <h2>age : {{ person.age }}</h2>
-        <button @click="changeName">changeName</button>
-        <button @click="changeAge">changeAge</button>
-        <button @click="changePerson">changePerson</button>
-        <hr>
-        <h2>{{ obj.a.b.c }}</h2>
-        <button @click="test">changePerson</button>
+        <h1>需求：水温达到50℃，或水位达到20cm，则联系服务器</h1>
+        <h2 id="demo">水温：{{ temp }}</h2>
+        <h2>水位：{{ height }}</h2>
+        <button @click="changePrice">水温+1</button>
+        <button @click="changeSum">水位+10</button>
     </div>
 </template>
-
-
-
+  
 <script lang="ts" setup name="Person">
+import { ref, watch, watchEffect } from 'vue'
+// 数据
+let temp = ref(0)
+let height = ref(0)
 
-import { reactive, watch } from 'vue'
-let person = reactive({
-    name: 'zhangsan',
-    age: 18,
-})
+// 方法
+function changePrice() {
+    temp.value += 10
+}
+function changeSum() {
+    height.value += 1
+}
 
-let obj = reactive({
-    a: {
-        b: {
-            c: 666
-        }
+// 用watch实现，需要明确的指出要监视：temp、height
+// watch([temp, height], (value) => {
+//     // 从value中获取最新的temp值、height值
+//     const [newTemp, newHeight] = value
+//     // 室温达到50℃，或水位达到20cm，立刻联系服务器
+//     if (newTemp >= 50 || newHeight >= 20) {
+//         console.log('联系服务器')
+//     }
+// })
+
+// 用watchEffect实现，不用
+// 返回一个stop的函数
+const stopWtach = watchEffect(() => {
+    // 室温达到50摄氏度，或水位达到20cm，立刻联系服务器
+    if (temp.value >= 50 || height.value >= 20) {
+        console.log(document.getElementById('demo')?.innerText)
+        console.log('联系服务器')
+    }
+    // 水温达到100，或水位达到50，取消监视
+    if (temp.value === 100 || height.value === 50) {
+        console.log('清理了')
+        stopWtach()
     }
 })
-
-
-function changeName() {
-    person.name += '-'
-}
-
-function changeAge() {
-    person.age += 1
-}
-
-function changePerson() {
-    Object.assign(person, { name: "lisi", age: 19 })
-
-}
-
-function test() {
-    obj.a.b.c = 999
-}
-
-// 默认深度watch
-watch(person, (newValue, oldValue) => {
-    console.log('person changed', newValue, oldValue)
-})
-// 修改对象的属性值，newValue和oldValue是同一个值
-
-
-watch(obj, (newValue, oldValue) => {
-    console.log('obj changed', newValue, oldValue)
-})
-// 修改对象的属性值，newValue和oldValue是同一个值
-
-
 </script>
-
-
-<style scoped >
-.person {
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 20px;
-}
-
-button {
-    margin: 0 5px;
-}
-</style>
