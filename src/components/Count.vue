5e04f5a28f2ab7413a97d5e3476498981e645c0f
ShengyunYu <yushengyun2013@163.com>
2024-01-19 23:26:01 +0800

    app vue

---
+++ src/components/Count.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="count">
+        <h2>sum: {{ sum }}</h2>
+        <select v-model.number="n">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>
+        <button @click="Add">+</button>
+        <button @click="Minus">-</button>
+    </div>
+</template>
+    
+<script setup lang='ts' name="Count">
+import { ref } from "vue"
+let sum = ref(1)
+let n = ref(1)
+
+function Add() {
+    sum.value += n.value
+}
+
+
+function Minus() {
+    sum.value -= n.value
+}
+</script>
+    
+<style scoped>
+.count {
+    background-color: aqua;
+    padding: 10px;
+    border-radius: 10px;
+    box-shadow: 0 0;
+}
+
+select,
+button {
+    margin: 0, 20px;
+    height: 25px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App) 
-app.use(router)
+const app = createApp(App)
 app.mount('#app')
