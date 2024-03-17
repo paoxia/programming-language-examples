3affb9512daa9380f210bf3905e591e62e9851b4
ShengyunYu <yushengyun2013@163.com>
2024-03-17 20:33:57 +0800

    threadlocal

--- src/main/java/org/saltedfish/concurrency/threadlocal/ThreadlocalDemo.java
+++ src/main/java/org/saltedfish/concurrency/threadlocal/ThreadlocalDemo.java
@@ -2,9 +2,41 @@ package org.saltedfish.concurrency.threadlocal;
 
 public class ThreadlocalDemo {
 
+    class ThreadA implements  Runnable {
+        private ThreadLocal<Integer> threadLocal;
+
+        ThreadA(ThreadLocal<Integer> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+        @Override
+        public void run() {
+            threadLocal.set(123);
+
+            System.out.println("ThreadA" + threadLocal.get());
+        }
+    }
+
+    class ThreadB implements Runnable {
+        private ThreadLocal<Integer> threadLocal;
+
+        ThreadB(ThreadLocal<Integer> threadLocal) {
+            this.threadLocal = threadLocal;
+        }
+        @Override
+        public void run() {
+            threadLocal.set(345);
+
+            System.out.println("ThreadB" + threadLocal.get());
+        }
+    }
 
 
     public static void main(String[] args) {
+        ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+        Thread threadA = new Thread(new ThreadlocalDemo().new ThreadA(threadLocal));
+        Thread threadB = new Thread(new ThreadlocalDemo().new ThreadB(threadLocal));
 
+        threadA.start();
+        threadB.start();
     }
 }
