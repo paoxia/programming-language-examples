b07defb8fdb72b81b0690f26d43bc573246dbf30
paoxia <yushengyun2013@163.com>
2024-04-22 00:14:49 +0800

    jedis

--- src/main/java/org/saltedfish/db/redis/RedisDemo.java
+++
@@ -1,4 +0,0 @@
-package org.saltedfish.db.redis;
-
-public class RedisDemo {
-}
---
+++ src/main/java/org/saltedfish/db/redisdemo/RedisDemo.java
@@ -0,0 +1,20 @@
+package org.saltedfish.db.redisdemo;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+public class RedisDemo {
+
+    public static void main(String[] args) {
+        // Jedis pool
+        JedisPool pool = new JedisPool("localhost", 6379);
+        Jedis jedis = pool.getResource();
+        if (jedis != null) {
+            jedis.set("clientName", "Jedis");
+
+            System.out.println(jedis.get("clientName"));
+        }
+
+
+    }
+}
