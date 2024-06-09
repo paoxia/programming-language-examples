d433eac92c5a416f1ca238650b22196968b77992
paoxia <yushengyun2013@163.com>
2024-06-09 12:00:05 +0800

    future

--- src/main/java/org/saltedfish/concurrency/future/FutureDemo.java
+++ src/main/java/org/saltedfish/concurrency/future/FutureDemo.java
@@ -1,7 +1,26 @@
 package org.saltedfish.concurrency.future;
 
-public class FutureDemo {
-    public static void main(String[] args) {
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+public class FutureDemo {
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Callable<Integer> callable = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return -1;
+            }
+        };
+
+        Future<Integer> future = executorService.submit(callable);
+
+        Integer res = future.get();
+        System.out.println(res);
     }
 }
